c50eb30440912046290b1fa032a0e4835bc8fd4d
Supun De Silva <supun1001@gmail.com>
2021-05-02 15:53:17 +1000

    Add the Result Getter
    Signed-off-by: Supun De Silva <supun1001@gmail.com>

---
+++ SQL-Scripts/004_GetAFLPredictions.sql
@@ -0,0 +1,33 @@
+CREATE OR ALTER VIEW [dbo].[GetAFLPredictions] AS
+
+	WITH CTE( MaxRunIdDt, [Week]) AS (SELECT DISTINCT
+		MAX(preds.[Run_Id_DateTime]) as MaxRunIdDt,
+		preds.[Week]
+	FROM [dbo].[AFL_Predictions] preds
+	GROUP BY preds.[Week])
+
+	SELECT
+		pred_Inputs.Run_Id,
+		pred_Inputs.Game,
+		pred_Inputs.[Date],
+		pred_Inputs.[Week],
+		pred_Inputs.Home_Team,
+		pred_Inputs.Away_Team,
+		pred_Inputs.Venue,
+		pred_Inputs.F_Home_Ground_Adv,
+		pred_Inputs.F_Away_Ground_Adv,
+		pred_Inputs.F_Last_5_h2h,
+		pred_Inputs.F_Last_5_h2h_In_Ground,
+		pred_Inputs.F_Season_Weighted_Last_5_h2h,
+		pred_Inputs.F_Margin_Weighted_Last_5_h2h,
+		pred_Inputs.F_Last_5_Home_Form,
+		pred_Inputs.F_Last_5_Home_Dominance,
+		pred_Inputs.F_Last_5_Away_Form,
+		pred_Inputs.F_Last_5_Away_Dominance,
+		pred_Inputs.F_Home_Odds,
+		pred_Inputs.F_Away_Odds,
+		preds.Prediction,
+		preds.Result
+	FROM [dbo].[AFL_Predictions] preds
+	INNER JOIN [dbo].[AFL_Prediction_Inputs] pred_Inputs ON pred_Inputs.game = preds.game and pred_Inputs.Run_Id_DateTime = preds.Run_Id_DateTime
+	INNER JOIN CTE ON CTE.MaxRunIdDt = pred_Inputs.Run_Id_DateTime
